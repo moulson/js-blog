11e6718ebd2ff37f6b74a7d6ac88abaf4b61e7a0
Daniel Moulson <dan24797@gmail.com>
2020-10-16 14:06:08 +0100

    initial

---
+++ blogs.js
@@ -0,0 +1,12 @@
+const BLOGS = [
+    {
+        'title': 'myblog',
+        'img' : 'https://via.placeholder.com/150x150',
+        'content': 'This is the first blog text'
+    },
+    {
+        'title': 'myblog2',
+        'img' : 'https://via.placeholder.com/150x150',
+        'content': 'This is the 2nd blog text'
+    }
+]
